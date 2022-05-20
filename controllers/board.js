228b9b84ebdc1d67db9e0baa119fa19c33da463f
kkh1902 <kkh1902@naver.com>
2022-05-20 22:53:48 +0900

    READ 기능 구현

--- controllers/board.js
+++ controllers/board.js
@@ -7,14 +7,44 @@ class board {
         try {
             const posts = await pool.query("select * from posts ");
 
-            return res.render("board", {
-                posts: posts[0],
-            });
+            return res.send(posts[0]);
         } catch (error) {
-            console.error(error);
-            return res.status(httpStatus.INTERNAL_SERVER_ERROR).send([]);
+            return res.status(500).json(error);
+        }
+    }
+
+    async postCreate(req, res) {
+        try {
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    }
+
+    async postInquery(req, res) {
+        try {
+            const { post_id } = req.params;
+            const post = await pool.query(
+                "select writer_id, post_title, post_content from posts where post_id = ? ",
+                [post_id]
+            );
+            return res.send(post[0]);
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    }
+
+    async postUpdate(req, res) {
+        try {
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    }
+
+    async postDelete(req, res) {
+        try {
+        } catch (error) {
+            return res.status(500).json(error);
         }
     }
 }
-
 module.exports = board;
--- routes/board.js
+++ routes/board.js
@@ -7,4 +7,17 @@ const board = new boardController();
 /* 글 목록 */
 router.get("/", board.inquery);
 
+/* 글 작성하기 */
+// router.get("/create", );  // 글 작성 페이지로 가라
+router.post("/post", board.postCreate);
+
+/* 글 상세 조회 */
+router.get("/post/:post_id", board.postInquery);
+
+/* 글 수정 */
+router.patch("/post", board.postUpdate);
+
+/* 글 삭제 */
+router.delete("/post", board.postDelete);
+
 module.exports = router;
